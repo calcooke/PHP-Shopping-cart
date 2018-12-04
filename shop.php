1cc53338cbafdfe8fbefdea53aef1f5d45ef8fdc
ziggyzulu <k00083370@student.lit.ie>
2018-12-04 17:03:55 +0000

    Making the product list

--- shop.php
+++ shop.php
@@ -3,3 +3,26 @@
 session_start();
 
 echo ("Welcome to the shop user " . $_SESSION['userID'] );
+
+$stmt = $conn->query("SELECT * FROM crm_database");
+
+?>
+
+<table style="border: 1px solid black">
+    <tr>
+        <th>Title</th>
+        <th>Edit</th>
+        <th>Delete</th>
+    </tr>
+    <?php
+    while ($row = $stmt->fetch()) {
+        ?>
+        <tr>
+            <td><?php echo $row['title']; ?></td>
+            <td><a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
+            <td><a href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+        </tr>
+        <?php
+    }
+    ?>
+</table>
