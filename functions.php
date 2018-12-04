fc0a2a7e294c442851e3a43fa7e9ea2d474862a1
ziggyzulu <k00083370@student.lit.ie>
2018-12-04 17:05:55 +0000

    Changing the functios back until i've decided how to go about it

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ session_start();
 include ('dbHandler.php');
 include('LoginHandler.php');
 
-if($_POST) {
+
 
     $name = $_POST['userName'];
     $pass = $_POST['userPassword'];
@@ -14,9 +14,9 @@ if($_POST) {
     $loginHandler = new LoginHandler();
     $loginHandler->userCheck($name, $pass, $conn);
 
-    die();
 
-}
+
+
 
 
 echo "Write more functions here";
